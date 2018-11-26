8d80afce247f7e01c999013fba62fa83b7d69c0a
Chun <caochun@gmail.com>
2018-11-26 23:01:11 +0800

    init

---
+++ CampusMap/CampusViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CampusViewController.swift
+//  CampusMap
+//
+//  Created by Chun on 2018/11/26.
+//  Copyright © 2018 Nemoworks. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class CampusViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func segValueChanged(_ sender: Any) {
+        
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
